4f95063d502c1fd8f16a7a71c0d26231f4499cc7
chainrain <292605957@qq.com>
2018-12-14 11:15:06 +0800

    6.实现管道模块

--- day13_ScrapyPlus/core/pipeline.py
+++ day13_ScrapyPlus/core/pipeline.py
@@ -1,3 +1,14 @@
 """
-管道模块
+管道模块: 处理提取出来的数据
 """
+class Pipeline(object):
+    def process_item(self,item,spider):
+        """
+        处理数据
+        :param item: 提取出来的数据
+        :param spider: 数据对应的爬虫
+        :return: item
+        """
+        # 简单打印一下
+        print(item.data)
+        return item
